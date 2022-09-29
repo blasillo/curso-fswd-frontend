e77b0d036bbc083827c7bf668115283cb9cb6052
btorregrosa <btorregrosa@infor.uva.es>
2022-09-29 20:42:30 +0200

    Inicio

--- src/app/modelos/usuario.ts
+++ src/app/modelos/usuario.ts
@@ -1,2 +1,12 @@
+import {Rol} from "./rol";
+
 export class Usuario {
+  // @ts-ignore
+  idUsuario: number;
+  nombreUsuario: string ="";
+  clave: string ="";
+  correo: string="";
+  // @ts-ignore
+  rol: Rol;
+  token: string="";
 }
