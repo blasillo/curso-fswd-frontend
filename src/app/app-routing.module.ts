772c87ded5e91b1dd3e4d39daf7a06788aa98aa0
btorregrosa <btorregrosa@infor.uva.es>
2022-09-29 20:45:59 +0200

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsuariosComponent} from "./usuarios/usuarios.component";
 
-const routes: Routes = [];
+const routes:Routes = [
+  { path:'', redirectTo: 'usuarios', pathMatch: 'full'},
+  { path:'usuarios', component: UsuariosComponent }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
